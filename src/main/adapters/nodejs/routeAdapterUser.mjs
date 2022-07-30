864872eef0a81c00e9ab7f9a4cd838a2fd63044a
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-29 21:32:00 -0300

    feature: now route adapter for node accepted body of request

--- src/main/adapters/nodejs/routeAdapterUser.mjs
+++ src/main/adapters/nodejs/routeAdapterUser.mjs
@@ -1,28 +1,58 @@
-// import { createServer } from 'http'
-// import { createServer } from ''
+import http, { IncomingMessage } from 'node:http'
+import { parse } from 'querystring'
 
-// import { RegisterUserController } from '../../../adapters/http/controllers/users/registerUserController'
-// import { AuthUserController } from '../../../adapters/http/controllers/users/authUserController'
-
-// import { IHttpRequest } from '../../../adapters/http/controllers/ports/http'
+// import { IHttpRequest } from '@src/adapters/http/controllers/ports/http'
+// import { AuthUserController } from '@src/adapters/http/controllers/users/authUserController'
+// import { RegisterUserController } from '@src/adapters/http/controllers/users/registerUserController'
 // import { ShowUserController } from '@src/adapters/http/controllers/users/showUserController'
 
-// export const adpterRoute = (controller:
-//   RegisterUserController |
-//   AuthUserController |
-//   ShowUserController
-// ): any => {
-//   return async (req: IncomingMessage, res: ServerResponse) => {
-//     const httpRequest: IHttpRequest = {
-//       body: req.complete,
-//       params: req.params
-//     }
+const server = http.createServer(async (request, res) => {
+  // set the request route
+  if (request.url === '/api' && request.method === 'POST') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    // res.write({
+    //   message: 'Hi there, This is a Vanilla Node.js API'
+    // })
 
-//     const httpResponse = await controller.handle(httpRequest)
+    let body = ''
 
-//     res.status(httpResponse.statusCode).json(httpResponse.body)
-//   }
-// }
+    request.on('data', (chunck) => {
+      body += chunck.toString()
+    })
+
+    request.on('end', () => {
+      console.log(body)
+      res.end(body)
+    })
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route not found' }))
+  }
+}).listen(3000)
+
+export const adpterRoute = (// controller:
+  // RegisterUserController |
+  // AuthUserController |
+  // ShowUserController
+) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/explicit-module-boundary-types
+  return async (request, response) => {
+    let body = ''
+    const httpRequest = {}
+
+    request.on('data', (chunck) => {
+      body += chunck
+    })
+
+    request.on('end', () => {
+      httpRequest.body = parse(body)
+      // httpRequest.params = request.params
+    })
+
+    // const httpResponse = await controller.handle(httpRequest)
+    // response.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
 
 process.on('uncaughtException', (error, origin) => {
   console.log(`${origin} signal received. \n${error}`)
