679df1714083fca91b2cc14d520a298c0a08b52c
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-02-15 20:55:07 -0300

    now is possible create client in ASAAS

--- src/main/adapters/express/routeAdapterToRegister.ts
+++ src/main/adapters/express/routeAdapterToRegister.ts
@@ -38,7 +38,27 @@ export const routeAdapterToRegister = (controller:
     }
 
     if (httpResponse.statusCode === 201) {
-      console.log(httpResponse)
+      httpResponse.body.password = '';
+
+      (async () => {
+        // eslint-disable-next-line no-undef
+        const data = await fetch(String(process.env.ASAAS_CUSTOMERS_SANDBOX), {
+          method: 'POST',
+          body: `{  "name": "${httpResponse.body.name}",
+          "email": "${httpResponse.body.email}",
+          "mobilePhone": "${httpResponse.body.mobilePhone}",
+          "cpfCnpj": "${httpResponse.body.cpfCnpj}",
+          "notificationDisabled": ${httpResponse.body.notificationDisabled}
+        }`,
+          headers: {
+            'Content-Type': 'application/json',
+            access_token: `${process.env.ASAAS_API_KEY_SANDBOX}`
+          },
+        })
+
+        console.log(data.status)
+      })()
+
       res.status(httpResponse.statusCode)
         .redirect(`${process.env.ADMIN}/login`)
     }
