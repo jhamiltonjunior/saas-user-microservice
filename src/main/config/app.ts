bb73aa8cc45df080afe5a608376c8b19672a995b
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-08-22 18:19:51 -0300

    Feat: cors added to use in frontend application

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,17 @@
 import express from 'express'
 import routes from './routes'
 import middlewares from './middlewares'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://blog.somedomain.com',
+    'http://loremipsum.com',
+  ]
+}))
 middlewares(app)
 routes(app)
 
